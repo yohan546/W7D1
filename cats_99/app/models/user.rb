d7859c8a10167cbacddefb47657fa55210746a3f
yohan oh <yohan.oh332@gmail.com>
2021-02-08 14:14:38 -0500

    made usertable and some methods in model

---
+++ cats_99/app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ApplicationRecord 
+    validates :username, :session_token, presence: true, uniqueness: true 
+    validates :password_digest, presence: true 
+
+    valdies :password, length: {minimum: 6, allow_nil: true}
+
+    attr_reader :password
+
+    before_validation :ensure_session_token 
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def reset_session_token! 
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save! 
+        self.session_token 
+    end
+
+    def check_password?(password)
+
+        password_object = BCrypt::Password.new(self.password_digest)
+
+        password_object.is_password?(password)
+    end
+
+
+
+
+end
